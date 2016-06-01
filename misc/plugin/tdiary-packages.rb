a788369b748a129744bebf8453fe1c4975839594
MATSUOKA Kohei <kohei@machu.jp>
2016-06-01 10:57:30 +0900

    add copyright

--- misc/plugin/tdiary-packages.rb
+++ misc/plugin/tdiary-packages.rb
@@ -1,3 +1,8 @@
+# tdiary-packages.rb: show tdiary package links
+# 
+# Copyright (c) MATSUOKA Kohei <http://www.machu.jp/>
+# Distributed under the GPL2 or any later version.
+
 enable_js('tdiary-packages.js')
 
 def tdiary_packages
